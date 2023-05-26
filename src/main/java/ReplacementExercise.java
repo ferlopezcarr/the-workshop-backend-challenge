bfadf71de1baa49898c6dc828ce5c5d7ebf92af7
Fernando López <ferlopcarr@gmail.com>
2023-05-26 12:48:00 +0200

    fix: :bug: replace function working and added more tests

--- src/main/java/ReplacementExercise.java
+++ src/main/java/ReplacementExercise.java
@@ -5,30 +5,40 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ReplacementExercise {
-  
-  /**
-   * String replacement
-   */
-  public static String replace(String source, String textToReplace, String replaceText) {
-    if (source == null ||  textToReplace == null || replaceText == null) {
-      throw new IllegalArgumentException("You provide a null parameters");
-    }
-    if (source.isEmpty() ||  textToReplace.isEmpty() || replaceText.isEmpty()) {
-      throw new IllegalArgumentException("You provide a empty string");
-    }
 
-    String replacedText = source;
-    int fromIndexToReplace = replacedText.indexOf(textToReplace);
-    boolean areTextReplacementMissing = true;
-    while (areTextReplacementMissing) {
-        int toIndex = textToReplace.length();
-        String firstReplacement = replacedText.substring(0, fromIndexToReplace);
-        String restOfTheText = replacedText.substring(toIndex, replacedText.length());
-        replacedText = firstReplacement + textToReplace + restOfTheText;
-        areTextReplacementMissing = restOfTheText.indexOf(textToReplace) > 0;
-    }
+    /**
+     *  Replace all occurrences of a text in a string with another text.
+     *  Example:
+     *      source: “Hello, hello!”
+     *      textToReplace: “lo”
+     *      replaceText: “1234”
+     *      returns: “Hel1234, hel1234!”
+     */
+    public static String replace(String source, String textToReplace, String replaceText) {
+        if (source == null ||  textToReplace == null || replaceText == null) {
+            throw new IllegalArgumentException("You provide a null parameters");
+        }
+        if (source.isEmpty() ||  textToReplace.isEmpty()) {
+            throw new IllegalArgumentException("You provide a empty string");
+        }
 
-    return replacedText;
-  }
+        String result = source;
+        int fromIndexToReplace = result.indexOf(textToReplace);
+        int currentIndex = fromIndexToReplace;
+        boolean areTextReplacementMissing = fromIndexToReplace > 0;
+        while (areTextReplacementMissing) {
+            int toIndex = fromIndexToReplace + textToReplace.length();
+            String textBeforeCurrentReplacement = result.substring(0, fromIndexToReplace);
+            String restOfTheText = result.substring(toIndex, result.length());
+            result = textBeforeCurrentReplacement + replaceText + restOfTheText;
+            currentIndex = fromIndexToReplace + replaceText.length();
+
+            int replacementRelativeIndexFound = restOfTheText.indexOf(textToReplace);
+            areTextReplacementMissing = replacementRelativeIndexFound > 0;
+            fromIndexToReplace = currentIndex + replacementRelativeIndexFound;
+        }
+
+        return result;
+    }
 
 }
--- src/test/java/ReplacementExerciseTest.java
+++ src/test/java/ReplacementExerciseTest.java
@@ -1,6 +1,7 @@
 package test.java;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import main.java.ReplacementExercise;
 import org.junit.jupiter.api.Test;
@@ -8,11 +9,6 @@ import org.junit.jupiter.api.Test;
 
 public class ReplacementExerciseTest {
 
-  /**
-  *     String src = “Hello, hello!”
-  *     String o = “lo”
-  *     String n = “1234”
-  */
   @Test
   public void itShouldReplaceOcurrenceWithText() {
     // Given
@@ -28,5 +24,98 @@ public class ReplacementExerciseTest {
     assertEquals(textWithReplacement, expectedResult);
   }
 
+  @Test
+  public void itShouldThrowIllegalArgumentExceptionWithNullSrcText() {
+    // Given
+    String src = null;
+    String textToReplace = "lo";
+    String replacementText = "1234";
+
+    // When & Then
+    assertThrows(IllegalArgumentException.class, () -> {
+      ReplacementExercise.replace(src, textToReplace, replacementText);
+    });
+  }
+
+  @Test
+  public void itShouldThrowIllegalArgumentExceptionWithNullTextToReplace() {
+    // Given
+    String src = "Hello, hello!";
+    String textToReplace = null;
+    String replacementText = "1234";
+
+    // When & Then
+    assertThrows(IllegalArgumentException.class, () -> {
+      ReplacementExercise.replace(src, textToReplace, replacementText);
+    });
+  }
+
+  @Test
+  public void itShouldThrowIllegalArgumentExceptionWithNullReplacementText() {
+    // Given
+    String src = "Hello, hello!";
+    String textToReplace = "lo";
+    String replacementText = null;
+
+    // When & Then
+    assertThrows(IllegalArgumentException.class, () -> {
+      ReplacementExercise.replace(src, textToReplace, replacementText);
+    });
+  }
+
+  @Test
+  public void itShouldThrowIllegalArgumentExceptionWithNEmptySrcText() {
+    // Given
+    String src = "";
+    String textToReplace = "lo";
+    String replacementText = "1234";
+
+    // When & Then
+    assertThrows(IllegalArgumentException.class, () -> {
+      ReplacementExercise.replace(src, textToReplace, replacementText);
+    });
+  }
+
+  @Test
+  public void itShouldThrowIllegalArgumentExceptionWithEmptyTextToReplace() {
+    // Given
+    String src = "Hello, hello!";
+    String textToReplace = "";
+    String replacementText = "1234";
+
+    // When & Then
+    assertThrows(IllegalArgumentException.class, () -> {
+      ReplacementExercise.replace(src, textToReplace, replacementText);
+    });
+  }
+
+  @Test
+  public void itShouldReplaceOcurrenceWithEmptyText() {
+    // Given
+    String src = "Hello, hello!";
+    String textToReplace = "lo";
+    String replacementText = "";
+
+    // When
+    String textWithReplacement = ReplacementExercise.replace(src, textToReplace, replacementText);
+
+    // Then
+    String expectedResult = "Hel, hel!";
+    assertEquals(textWithReplacement, expectedResult);
+  }
+
+  @Test
+  public void itShouldNotReplaceAnythingWithNoOccurrences() {
+    // Given
+    String src = "Hell, hell!";
+    String textToReplace = "lo";
+    String replacementText = "";
+
+    // When
+    String textWithReplacement = ReplacementExercise.replace(src, textToReplace, replacementText);
+
+    // Then
+    assertEquals(textWithReplacement, src);
+  }
 
 }
